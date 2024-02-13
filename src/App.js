8078634916ed8bdd17147f18bd984e8a42507992
dhruvilmiyani <dhruvilmiyani8888@gmail.com>
2024-02-13 14:48:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           Welcome To CI/CD Pipeline
+          CI/CD Pipeline
         </a>
       </header>
     </div>
