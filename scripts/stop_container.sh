1c406ff0adcc9b47e4ca3440266250697b36d221
dhruvilmiyani <dhruvilmiyani8888@gmail.com>
2024-02-14 09:48:56 +0530

    done

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,9 +3,11 @@ set -e
 
 # Stop the running container (if any)
 containerid=$(sudo docker ps -q)
+imageid=$(sudo docker images -q)
 
-if [ -n "$containerid" ]; then
+if [ -n "$containerid"] || [ -n "$imageid" ] ; then 
     sudo docker rm -f "$containerid"
+    sudo docker rmi -f "$imageid"
 else
     echo "No container running"
 fi
