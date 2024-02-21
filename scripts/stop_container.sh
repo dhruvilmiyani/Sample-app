01d5cc263f0f29adc22b48723b3cc322f2294cd9
dhruvilmiyani <dhruvilmiyani8888@gmail.com>
2024-02-21 16:30:51 +0530

    done

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,8 +1,8 @@
 set -e
 
 # Stop the running container (if any)
-#sudo docker rm -f $(sudo docker ps -a -q)
-#sudo docker rmi $(docker images -f "dangling=true" -q)
+sudo docker rm -f $(sudo docker ps -a -q)
+sudo docker rmi $(docker images -f "dangling=true" -q)
 
     
 
