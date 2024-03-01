357142d7a2f14d985e721d12540c0b9de48afaa9
dhruvilmiyani <dhruvilmiyani8888@gmail.com>
2024-03-01 12:24:30 +0530

    done

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,7 +5,7 @@ set -e
 docker pull dhruvilmiyani/dhruvil0152
 
 # Run the Docker image as a container
-docker run -d -p 3001:3001 dhruvilmiyani/dhruvil0152
+docker run -d -p 3000:3000 dhruvilmiyani/dhruvil0152
 
 
 
