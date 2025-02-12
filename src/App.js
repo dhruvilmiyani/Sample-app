469d75d2fe522a2e79fa0b757e6175ec30eaabb7
dhruvilmiyani <dhruvilmiyani8888@gmail.com>
2025-02-12 17:03:22 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          welcome to CI-CD Pipelines final-01522222 
+          welcome to jenkins CI/CD pipeline.
         </a>
       </header>
     </div>
